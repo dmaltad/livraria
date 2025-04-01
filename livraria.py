589e4573f137dddfde79c7932ac0033a66286c7c
Daniel Malta <116168952+dmaltad@users.noreply.github.com>
2025-04-01 20:04:21 -0300

    Update livraria.py

--- livraria.py
+++ livraria.py
@@ -1,37 +1,37 @@
 class itemNaoEncontradoException(Exception):
-    print("Item não encontrado!")
+    pass
 
 class estoqueInsuficienteException(Exception):
-    print("Estoque insuficiente!")
+    pass
 
 class estoque:
-    _quantidade = None
+    _itens = None
 
-    def __init__(self, quantidade):
-        self._quantidade = quantidade
 
-class livro(estoque):
-    _titulo = None
-    _autor = None
-    _preco = None
-
-    def __init__(self, titulo, autor, quantidade, preco):
-        super().__init__(quantidade)
-        self._titulo = titulo
-        self._autor = autor
-        self._preco = preco
-        self._quantidade = quantidade
+    def __init__(self, itens):
+        self._itens = itens
 
     def processarVenda(self, titulo, quantidade):
-        if self._titulo == titulo:
-            if self._quantidade >= quantidade and self._quantidade > 0:
-                self._quantidade -= quantidade
-                total = quantidade * self._preco
+        if titulo in self._itens:
+            quantidadeEmEstoque = self._itens[titulo]["quantidade"]
+            if quantidadeEmEstoque >= quantidade and quantidadeEmEstoque > 0:
+                self._itens[titulo]["quantidade"] -= quantidade
+                total = quantidade * self._itens[titulo]["livro"].preco
                 return total
             else:
-                print("Estoque insuficiente!")
+                raise estoqueInsuficienteException("Estoque insuficiente!")            
         else:
-            print("Item não encontrado!") 
-        
-livro1 = livro("O", "J.R.R.Tokien", 30, 60)
-livro1.processarVenda("O", 2)
+            raise itemNaoEncontradoException("Item não econtrado no estoque!")
+    
+class livro:
+    _titulo = None
+    _autor = None
+    preco = None
+
+    def __init__(self, titulo, autor, preco):
+        self._titulo = titulo
+        self._autor = autor
+        self.preco = preco
+
+estoque1 = estoque({"O senhor dos aneis":{"livro":livro("O senhor dos aneis", "J.R.R.Tokien", 60),"quantidade":30}})
+print(estoque1.processarVenda("O senhor dos aneis", 3))
